fcf0080b219f25a18239f339e79af33ddfe6eb22
charlesfedrixz <chandtekcham@gmail.com>
2024-10-11 18:21:21 +0530

    port update

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ app.get("/", (req, res) => {
   });
 });
 
-const port = 3002;
+const port = 8080;
 server.listen(port, () => {
   console.log(
     `Server of your Smart Restaurant is running on http://localhost:${port}`
