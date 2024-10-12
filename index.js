f4426b44fb70eb484579c5d386acabd466aaadc2
charlesfedrixz <chandtekcham@gmail.com>
2024-10-12 10:49:27 +0530

    updated port to 3002

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ app.get("/", (req, res) => {
   });
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3002;
 server.listen(port, () => {
   console.log(
     `Server of your Smart Restaurant is running on http://localhost:${port}`
