c793fb98999b1bcd9c33576ac2a03ab1847b6791
charlesfedrixz <chandtekcham@gmail.com>
2024-10-22 12:04:36 +0530

    Deleted unnecessary files

--- utils/errorHandler.js
+++
@@ -1,8 +0,0 @@
-class throwError extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = statusCode || 500; // Default to Internal Server Error if status code is not provided
-  }
-}
-module.exports = throwError;
