385a1f2db43e56c1384fcfe1d3d5d96fd0350a48
nganaremba-rem <ngancloverr+remkugithub@gmail.com>
2025-01-24 19:35:21 +0530

    ecosystem ignored

--- config/db.js
+++ config/db.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      ssl: true,
-      tls: true,
-      tlsAllowInvalidCertificates: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
